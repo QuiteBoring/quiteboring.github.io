47ca23e42f432407a403380ee8ae870f867291a5
Nathan <103887913+QuiteBoring@users.noreply.github.com>
2025-03-17 09:32:17 -0400

    Update navbar.tsx

--- app/cobalt/components/navbar.tsx
+++ app/cobalt/components/navbar.tsx
@@ -15,73 +15,71 @@ export default function Navbar() {
         C O B A L T
       </Link>
 
-      <div className="flex items-center">
-        <div className="hidden md:flex space-x-4 mr-3 sm:mr-5">
-          <Link
-            href="/cobalt/roadmap"
-            className="text-zinc-300 hover:text-white transition duration-300 flex items-center"
-          >
-            <FaMapLocationDot className="mr-2" />
-            Roadmap
-          </Link>
-          <Link
-            href="/cobalt/about"
-            className="text-zinc-300 hover:text-white transition duration-300 mr-5 flex items-center"
-          >
-            <FaCircleUser className="mr-2" />
-            About Us
-          </Link>
-          <Link
-            href="https://discord.gg/cobaltqol"
-            target="_blank"
-            className="text-zinc-300 hover:text-white transition duration-300 mr-5 flex items-center"
-          >
-            <FaDiscord className="mr-2" />
-            Discord
-          </Link>
-          <Link
-            href="https://github.com/CobaltQOL"
-            target="_blank"
-            className="text-zinc-300 hover:text-white transition duration-300 mr-5 flex items-center"
-          >
-            <FaGithub className="mr-2" />
-            Github
-          </Link>
-        </div>
-
-        <button
-          className="md:hidden text-zinc-300 hover:text-white transition duration-300 mr-3 relative h-6 w-6 z-50"
-          onClick={toggleMenu}
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+      <div className="hidden md:flex items-center space-x-4 mr-3 sm:mr-5">
+        <Link
+          href="/cobalt/roadmap"
+          className="text-zinc-300 hover:text-white transition duration-300 flex items-center"
         >
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
-            <span
-              className={`block absolute h-[2px] w-full bg-current transition-transform duration-300 ${
-                isMenuOpen ? "rotate-45" : "-translate-y-1.5"
-              }`}
-            />
-            <span
-              className={`block absolute h-[2px] w-full bg-current transition-opacity duration-300 ${
-                isMenuOpen ? "opacity-0" : ""
-              }`}
-            />
-            <span
-              className={`block absolute h-[2px] w-full bg-current transition-transform duration-300 ${
-                isMenuOpen ? "-rotate-45" : "translate-y-1.5"
-              }`}
-            />
-          </div>
-        </button>
+          <FaMapLocationDot className="mr-2" />
+          Roadmap
+        </Link>
+        <Link
+          href="/cobalt/about"
+          className="text-zinc-300 hover:text-white transition duration-300 flex items-center"
+        >
+          <FaCircleUser className="mr-2" />
+          About Us
+        </Link>
+        <Link
+          href="https://discord.gg/cobaltqol"
+          target="_blank"
+          className="text-zinc-300 hover:text-white transition duration-300 flex items-center"
+        >
+          <FaDiscord className="mr-2" />
+          Discord
+        </Link>
+        <Link
+          href="https://github.com/CobaltQOL"
+          target="_blank"
+          className="text-zinc-300 hover:text-white transition duration-300 flex items-center"
+        >
+          <FaGithub className="mr-2" />
+          Github
+        </Link>
       </div>
 
+      <button
+        className="md:hidden text-zinc-300 hover:text-white transition duration-300 mr-3 relative h-6 w-6 z-50"
+        onClick={toggleMenu}
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+      >
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+          <span
+            className={`block absolute h-[2px] w-full bg-current transition-transform duration-300 ${
+              isMenuOpen ? "rotate-45" : "-translate-y-1.5"
+            }`}
+          />
+          <span
+            className={`block absolute h-[2px] w-full bg-current transition-opacity duration-300 ${
+              isMenuOpen ? "opacity-0" : ""
+            }`}
+          />
+          <span
+            className={`block absolute h-[2px] w-full bg-current transition-transform duration-300 ${
+              isMenuOpen ? "-rotate-45" : "translate-y-1.5"
+            }`}
+          />
+        </div>
+      </button>
+
       <div
-        className={`fixed top-0 left-0 right-0 w-full md:hidden bg-slate-800 backdrop-blur-sm border-b border-slate-900/25 shadow-xl transition-all duration-300 z-40 ${
+        className={`fixed top-16 left-0 right-0 w-full md:hidden bg-slate-800 backdrop-blur-sm border-b border-slate-900/25 shadow-xl transition-all duration-300 z-40 ${
           isMenuOpen
             ? "opacity-100 visible translate-y-0"
             : "opacity-0 invisible -translate-y-full"
         }`}
       >
-        <div className="flex flex-col p-4 space-y-5 pt-16">
+        <div className="flex flex-col p-4 space-y-5">
           <Link
             href="/cobalt"
             className="text-zinc-300 hover:text-white transition duration-300 text-left w-full pl-[2%] flex items-center"
