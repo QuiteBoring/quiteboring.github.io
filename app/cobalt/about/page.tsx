09e9ad239607414d241dbe44dcf6ce38f992a186
Nathan <103887913+QuiteBoring@users.noreply.github.com>
2025-03-23 20:16:32 -0400

    so true.

--- app/cobalt/about/page.tsx
+++ app/cobalt/about/page.tsx
@@ -39,49 +39,8 @@ export default function AboutUs() {
           <div className="flex flex-wrap justify-center gap-4">
             <User
               name="Nathan"
-              description="Mentally Unstable"
+              description="Lone Alpha Wolf"
               image="https://cdn.discordapp.com/avatars/1324116012240732172/b392359380aab7fda7630060c6be2e12.png"
-            />
-            <User
-              name="Twiston"
-              description="Quality shitcode 🐸"
-              image="https://cdn.discordapp.com/avatars/855798460593733652/79549714d67ed70ac29c71fb258d9f29.png"
-            />
-            <User
-              name="Night"
-              description="Can barely play skyblock"
-              image="https://cdn.discordapp.com/avatars/452076196419600394/004771eef61f8884ecfb09a40636e0ba.png"
-            />
-            <User
-              name="Nima"
-              description="Long live Germany!"
-              image="https://cdn.discordapp.com/avatars/697415764306821141/5b26ed78e66734e98ec070c95b3e7762.png"
-            />
-          </div>
-
-          <div className="w-[70%] mt-6 mb-4 h-px bg-gray-400 opacity-50"></div>
-
-          <h1 className="text-3xl font-normal text-white mb-3">Contributors</h1>
-          <div className="flex flex-wrap justify-center gap-4">
-            <User
-              name="Kessu"
-              description="Emotional Support"
-              image="https://cdn.discordapp.com/avatars/618823461938135090/bc39e7371900ada8bd1f4af581c9a2f9.png"
-            />
-            <User
-              name="Funzies"
-              description="Professional bullshitter"
-              image="https://cdn.discordapp.com/avatars/1176527774597521448/7260300494e8c870933a4a6cbf019ab0.png"
-            />
-            <User
-              name="Kobe"
-              description="Helped with original website"
-              image="https://cdn.discordapp.com/avatars/1187752214429712508/c56ede65ab9588869a61b94374f4536a.png"
-            />
-            <User
-              name="Flow"
-              description="Best critiques!"
-              image="https://cdn.discordapp.com/avatars/1300063261609295932/28a2a45a6e3c2086568b41f72e5449b7.png"
             />
           </div>
         </div>
